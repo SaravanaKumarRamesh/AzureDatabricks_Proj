2d3697d2c5df6787f83afd8d569848031ec8a1ca
SaravanaKumarRamesh <61381667+SaravanaKumarRamesh@users.noreply.github.com>
2022-08-16 17:41:37 +0530

    Azure databricks (#2)
    * Initial Commit
    
    * Update:
    README.md
    notebooks
    
    * Update:
    README.md
    notebooks
    
    * Delete notebooks/Users/saravana.k.ramesh@accenture.com/Formula1_project directory
    
    * Update:
    README.md
    notebooks
    
    * Update:
    README.md
    notebooks
    
    * Update:
    README.md
    notebooks

---
+++ notebooks/Users/Formula1_project/3. Data_ingestion_Constructors.py
@@ -0,0 +1,31 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ###### Ingest Constructors data from Azure Storage ######
+
+# COMMAND ----------
+
+# DBTITLE 1,Create Schema and ingest data into the data frame
+from pyspark.sql.types import IntegerType,StringType,StructType,StructField
+from pyspark.sql.functions import col,current_timestamp
+Constructors_Schema  = StructType(fields = [StructField("constructorId",IntegerType(),False),
+                                            StructField("constructorRef",StringType(),False),
+                                            StructField("name",StringType(),False),
+                                            StructField("nationality",StringType(),False),
+                                            StructField("url",StringType(),False)])
+
+Constructor_Df = spark.read.schema(Constructors_Schema).json("/mnt/raw/constructors.json")
+
+# COMMAND ----------
+
+# DBTITLE 1,Rename Columns and remove columns
+Constructor_renamedf = Constructor_Df.withColumnRenamed("constructorId","constructor_Id")\
+                                        .withColumnRenamed("constructorRef","constructor_Ref")\
+                                        .withColumn("Ingestion_time",current_timestamp())\
+                                        .drop(col("url"))
+
+# COMMAND ----------
+
+Constructor_renamedf.write.mode("overwrite").parquet("dbfs:/mnt/processed/Constructors")
+
+# COMMAND ----------
+
---
+++ notebooks/Users/Formula1_project/4. Data_Ingestion_drivers.py
@@ -0,0 +1,49 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #### Data Ingestion from Drivers file #####
+
+# COMMAND ----------
+
+# DBTITLE 1,Functions to be imported
+from pyspark.sql.types import IntegerType,StringType,StructType,StructField,DateType
+from pyspark.sql.functions import col,current_timestamp,concat,lit
+
+# COMMAND ----------
+
+# DBTITLE 1,define a Schema and import the file
+Drivers_Schema = StructType(fields = [ StructField ("driverId",IntegerType(),False),
+                                        StructField ("driverRef",StringType(),False),
+                                        StructField ("number",IntegerType(),True),
+                                        StructField ("code",StringType(),False),
+                                        StructField ("name",StructType(fields = [ StructField ("forename",StringType(),False),StructField ("surname",StringType(),False)]),False),
+                                        StructField ("dob",DateType(),False),
+                                        StructField ("nationality",StringType(),False),
+                                        StructField ("url",StringType(),False)
+                                    
+])
+
+Drivers_Df = spark.read\
+            .schema(Drivers_Schema)\
+            .json("/mnt/raw/drivers.json")
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Transform the Dataframe
+Drivers_transform_df = Drivers_Df.withColumnRenamed("driverId","driver_Id")\
+                                .withColumnRenamed("driverRef","driver_Ref")\
+                                .withColumn("name",concat(col("name.forename"), lit(" "), col("name.surname")))\
+                                .withColumn("Ingestion_time",current_timestamp())\
+                                .drop(col("url"))
+
+# COMMAND ----------
+
+# DBTITLE 1,Export it to a parquet file
+Drivers_transform_df.write.mode("overwrite").parquet("dbfs:/mnt/processed/Drivers")
+
+# COMMAND ----------
+
+display(Drivers_transform_df)
+
+# COMMAND ----------
+
