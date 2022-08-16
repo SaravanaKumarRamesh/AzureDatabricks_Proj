702875da4eda7a0dee1f9c0b9d40afe02b3d5fe9
SaravanaKumarRamesh <61381667+SaravanaKumarRamesh@users.noreply.github.com>
2022-08-16 22:10:51 +0530

    Update: README.md notebooks

---
+++ notebooks/Users/Formula1_project/Ingestion/7.Data_ingestion_Laptimes.py
@@ -0,0 +1,41 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ##### Data Ingestion of laptimes #####
+
+# COMMAND ----------
+
+# DBTITLE 1,Import Functions 
+from pyspark.sql.types import IntegerType,StructType,StructField,StringType
+from pyspark.sql.functions import *
+
+# COMMAND ----------
+
+laptimes_Schema = StructType(fields = [ StructField ("raceId",IntegerType(),False),
+                                        StructField ("driverId",IntegerType(),False),
+                                        StructField ("lap",IntegerType(),False),
+                                        StructField ("position",IntegerType(),False),
+                                        StructField ("time",StringType(),False),
+                                        StructField ("milliseconds",IntegerType(),False)
+                                    
+])
+
+laptimes_Df = spark.read\
+            .schema(laptimes_Schema)\
+            .csv("/mnt/raw/lap_times")
+
+# COMMAND ----------
+
+Laptimes_transform_df = laptimes_Df.withColumnRenamed("raceId","race_Id")\
+                                .withColumnRenamed("driverId","driver_Id")\
+                                .withColumn("Ingestion_time",current_timestamp())
+
+# COMMAND ----------
+
+Laptimes_transform_df.write.mode("overwrite").parquet("dbfs:/mnt/processed/lap_times")
+
+# COMMAND ----------
+
+display(Laptimes_transform_df)
+
+# COMMAND ----------
+
